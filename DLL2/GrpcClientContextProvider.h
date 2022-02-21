531414277123ee5a38a73718183f391a1c46d7b4
Simon Maurer <simon.maurer@zeiss.com>
2022-02-21 13:55:04 +0100

    Added comment about the motivation of the current class and library layout.

--- DLL2/GrpcClientContextProvider.h
+++ DLL2/GrpcClientContextProvider.h
@@ -5,6 +5,12 @@
 #include "dll2_export.h"
 
 namespace DLL2 {
+  //The actual motivation to create a class like this in a separate DLL, was to
+  //abstract the cancellation concept in a way that can be used in several gRPC-based client DLLs.
+  //In actual production code such a class might provide a bridge between
+  //the cancellation concept of the main software (e.g., polling some atomic bool)
+  //and gRPC (calling ClientContext::TryCancel()). There might be other use-cases as well.
+  //For the sake of simplicity, the example was reduced to the bare minimum.
   class DLL2_EXPORT ClientContextProvider
   {
   public:
